a6da267bf1f24c3ad8d6a2c99b8e769a094ac397
hoanganh732 <85354504+hoanganh732@users.noreply.github.com>
2025-06-15 10:24:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api');
+//const TelegramBot = require('node-telegram-bot-api');
 const TelegramBot = require('node-telegram-bot-api');
 const foodOptions = require('./food-options');
 const people = require('./people');
