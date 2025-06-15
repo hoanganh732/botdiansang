8cddaceb21446fe16b76df8b3314dcb1231af280
hoanganh732 <85354504+hoanganh732@users.noreply.github.com>
2025-06-15 12:19:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 const GROUP_ID = process.env.GROUP_ID;
-const POLL_TIME = '12:11';
-const REMINDER_TIME = '12:11';
+const POLL_TIME = '12:21';
+const REMINDER_TIME = '12:21';
 
 function sendPoll() {
   const todayIndex = getTodayIndex();
